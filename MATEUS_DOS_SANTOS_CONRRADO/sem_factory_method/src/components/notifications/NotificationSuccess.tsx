525999caca129fe41e0dcc236aa691fcec849e80
Msconrrado <mateusconrado01@gmail.com>
2025-05-26 15:29:16 -0300

    Envio do código sem factory method

---
+++ MATEUS_DOS_SANTOS_CONRRADO/sem_factory_method/src/components/notifications/NotificationSuccess.tsx
@@ -0,0 +1,23 @@
+import { Check, X } from "lucide-react";
+
+interface NotificationSuccessProps {
+  message: string;
+  closeNotification: () => void;
+}
+
+export function NotificationSuccess({ message, closeNotification }: NotificationSuccessProps) {
+  return (
+    <div className={`fixed bg-black/80 w-full h-full flex items-center justify-center`}>
+      <div className="relative bg-green-50 w-full max-w-2xl h-full max-h-80 rounded-lg p-8 flex flex-col items-center justify-center border-2 border-green-600">
+        <div className="absolute top-0 left-0 flex items-center justify-between w-full p-4">
+          <div className="flex items-center gap-1">
+            <p className="font-medium text-green-600 text-lg">Sucesso</p>
+            <Check className="text-green-600" />
+          </div>
+          <X className="cursor-pointer" onClick={closeNotification} />
+        </div>
+        <h2 className="text-2xl font-normal text-center">{message}</h2>
+      </div>
+    </div>
+  )
+}
